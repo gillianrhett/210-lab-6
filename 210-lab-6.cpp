af85fba53d27fcf1036dbb0ab25f616494c3e24c
gillianrhett <gillianrhett@gmail.com>
2026-02-15 11:16:58 -0800

    initial commit

---
+++ 210-lab-6.cpp
@@ -0,0 +1,20 @@
+// COMSC-210 | Lab 6 | Gillian Rhett
+#include <iostream>
+using namespace std;
+
+void enterArrayData(double*);
+void outputArrayData(double*);
+double sumArray(double*);
+
+const int arrSize = 5;
+
+main() {
+
+}
+
+void enterArrayData(double* arr){
+// receives a pointer to an array of doubles
+//  and appends values entered by user
+
+    cout << "Data entry for the array:" << endl;
+}
