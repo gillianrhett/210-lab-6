c242a7f04cae69f61dcc57c04b32a2b7afe7035c
gillianrhett <gillianrhett@gmail.com>
2026-02-15 11:28:00 -0800

    wrote enterArrayData()

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -9,12 +9,39 @@ double sumArray(double*);
 const int arrSize = 5;
 
 main() {
+    double* myArr = new double[arrSize];
+    
+    enterArrayData(myArr);
 
+    delete myArr;
+    myArr = nullptr;
 }
 
 void enterArrayData(double* arr){
-// receives a pointer to an array of doubles
+// takes a pointer to an array of doubles
 //  and appends values entered by user
 
     cout << "Data entry for the array:" << endl;
+    double input = 0.0;
+    for (int i = 0; i < arrSize; ++i) {
+        cout << " > Element #" << i + 1 << ": ";
+        cin >> *(arr + i);
+    }
+    
+    cout << "\nData entry complete." << endl;
+}
+
+void outputArrayData(double* arr) {
+// takes a pointer to an array of doubles
+//  and displays the elements
+    
+}
+
+double sumArray(double* arr) {
+// takes a pointer to an array of doubles
+//  and adds them all and returns the sum
+
+    double sum = 0;
+
+    return sum;
 }
